1ae5d33ad5c3ab77cea5448ae95a1a4a4307d90b
dnovakovic15 <dnovakovic21@yahoo.com>
2017-10-24 18:46:21 -0500

    Maps API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,41 +11,41 @@ var markers = [
 
 var bounds = new google.maps.LatLngBounds();
 
-$(document).ready(function(){
-    initialize();
-})
+// $(document).ready(function(){
+//     initialize();
+// })
 
-function initialize() {
-  pyrmont = new google.maps.LatLng(-34.397, 150.644);
+// function initialize() {
+//   pyrmont = new google.maps.LatLng(-34.397, 150.644);
 
-  map = new google.maps.Map(document.getElementById('map'), {
-      center: pyrmont,
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP,
-    });
+//   map = new google.maps.Map(document.getElementById('map'), {
+//       center: pyrmont,
+//       zoom: 8,
+//       mapTypeId: google.maps.MapTypeId.ROADMAP,
+//     });
 
-  var request = {
-    location: pyrmont,
-    radius: '500000',
-    type: ['restaurant']
-  };
+//   var request = {
+//     location: pyrmont,
+//     radius: '500000',
+//     type: ['restaurant']
+//   };
 
-  service = new google.maps.places.PlacesService(map);
-  service.nearbySearch(request, callback);
-}
+//   service = new google.maps.places.PlacesService(map);
+//   service.nearbySearch(request, callback);
+// }
 
-function callback(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.OK) {
-    for (var i = 0; i < results.length; i++) {
-      var place = results[i];
-      console.log(results[i]);
-      console.log(results[i].name);
-      $('#map').append(results[i].name);
-      console.log('location: ' + results[i].geometry.viewport.b.b);
-      createMarker(results[i]);
-    }
-  }
-}
+// function callback(results, status) {
+//   if (status == google.maps.places.PlacesServiceStatus.OK) {
+//     for (var i = 0; i < results.length; i++) {
+//       var place = results[i];
+//       console.log(results[i]);
+//       console.log(results[i].name);
+//       $('#map').append(results[i].name);
+//       console.log('location: ' + results[i].geometry.viewport.b.b);
+//       createMarker(results[i]);
+//     }
+//   }
+// }
 
 // function createMarker(location){
 //     for( i = 0; i < markers.length; i++ ) {
@@ -70,46 +70,48 @@ function callback(results, status) {
 //     }
 // }
 
-// function geoCoding(location){
-//     queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=+evanston&key='+apiKey;
+function geoCoding(location){
+    queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=+evanston&key='+apiKey;
 
-//     $.ajax({
-//             url : queryURL,
-//             method : "GET",
-//             xhrFields: {
-//                 withCredentials: true
-//             },
-//         })
-//         .done(function(response){
-//             console.log(response.results[0].geometry.location);
-//         });
-// }
+    $.ajax({
+            url : queryURL,
+            method : "GET",
+            xhrFields: {
+                withCredentials: true
+            },
+        })
+        .done(function(response){
+            console.log(response.results[0].geometry.location);
+        });
+}
 
-// directions(location);
+directions();
 
-// function directions(location){
-//     var directionsService = new google.maps.DirectionsService;
-//     var directionsDisplay = new google.maps.DirectionsRenderer;
 
-//     calculateAndDisplayRoute(directionsService, directionsDisplay);
-// }
+function directions(){
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
 
-// function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-//     directionsService.route({
-//         origin: 'chicago',
-//         destination: 'evanston',
-//         travelMode: 'DRIVING'
-//     }, 
-//     function(response, status) {
-//         if (status === 'OK') {
-//             directionsDisplay.setDirections(response);
-//         } else {
-//             window.alert('Directions request failed due to ' + status);
-//         }
-//     });
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+}
 
-//     directionsDisplay.setMap(map);
-// }
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+    directionsService.route({
+        origin: 'chicago',
+        destination: 'evanston',
+        travelMode: 'DRIVING'
+    }, 
+    function(response, status) {
+        if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+            console.log(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+
+    directionsDisplay.setMap(map);
+}
 
 
 // currentLocation();
